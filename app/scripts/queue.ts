235be992d731d579cfda6f14d25d630d2172178e
Simon Martin <smartin@scottlogic.co.uk>
2016-05-30 13:33:20 +0100

    playing with router, canned data

--- app/scripts/queue.ts
+++ app/scripts/queue.ts
@@ -11,53 +11,73 @@ import {Entry} from './queue_entry'
 
 function handleCheckChange(id) {
     console.log('change', id);
+    ReactRouter.browserHistory.push(`/test/${id}`)
 }
 
-const queue = [
-    {
-        id: 10, text: 'Hello'
-    }
-    , {
-        id: 11, text: 'Hi'
-    }
-    , {
-        id: 12, text: 'Bob'
-    }
+interface QueueEntry {
+    id: number,
+    selected?: boolean,
+    text: string
+}
+const queue: Array<QueueEntry> = [
+    { id: 1, selected: true, text: 'Literal html' }
+    , { id: 2, selected: true, text: 'Literal html' }
+    , { id: 3, selected: true, text: 'Literal html' }
+    , { id: 4, selected: true, text: 'Literal html' }
+    , { id: 5, selected: true, text: 'Literal html' }
+    , { id: 6, selected: true, text: 'Literal html' }
+    , { id: 7, selected: true, text: 'Literal html' }
+    , { id: 8, selected: true, text: 'Literal html' }
+    , { id: 9, selected: true, text: 'Literal html' }
+    , { id: 10, text: 'Hello' }
+    , { id: 11, text: 'Hi' }
+    , { id: 12, text: 'Bob' }
 ]
 
-export class Main extends React.Component<{}, {}> {
+interface HistoryProps {
+    history: HistoryModule.HistoryQueries
+}
+export class Main extends React.Component<HistoryProps, {}> {
     render() {
         return (
             h('div.hello',
                 h('h1', 'Main Page'),
-                h('h2', 'Test content' )
+                h('h2', 'Test content'),
+                h(ReactRouter.Link, { to: '/test' }, 'main')
             )
         )
     }
 }
-
+export class Test extends React.Component<HistoryProps, {}> {
+    render() {
+        return (
+            h('div.hello',
+                h('h1', 'Test Page'),
+                h('h2', 'Test content'),
+                h('ul',
+                    h('li', h(ReactRouter.Link, { to: '/' }, 'main')),
+                    h('li', h(ReactRouter.Link, { to: 'b' }, 'b')),
+                    h('li', h(ReactRouter.Link, { to: 'test/a/b/c'}, 'deeper page'))
+                )
+            )
+        )
+    }
+}
 ReactDOM.render(
     h('div',
-        queue.map((entry) => {
+        h(ReactRouter.Router, { history: ReactRouter.browserHistory },
+            h(ReactRouter.Route, { path: '/', component: Main }),
+            h(ReactRouter.Route, { path: '/test', component: Test })
+        )
+        , queue.map((entry) => {
             return h(Entry,
                 {
                     key: entry.id,
                     id: entry.id,
-                    selected: false,
-                    onCheckChange: (id) => { console.log('change', id) }
+                    selected: entry.selected || false,
+                    onCheckChange: (id) => handleCheckChange(id)
                 }, `${entry.text}`)
         })
-        , h(Entry, { key: 1, id: 1, selected: false, onCheckChange: (id) => { console.log('change', id) } }, 'Literal html')
-        , h(Entry, { key: 2, id: 2, selected: false, onCheckChange: (id) => { console.log('change', id) } }, 'Literal html')
-        , h(Entry, { key: 3, id: 3, selected: false, onCheckChange: (id) => { console.log('change', id) } }, 'Literal html')
-        , h(Entry, { key: 4, id: 4, selected: false, onCheckChange: (id) => { console.log('change', id) } }, 'Literal html')
-        , h(Entry, { key: 5, id: 5, selected: false, onCheckChange: (id) => { console.log('change', id) } }, 'Literal html')
-        , h(Entry, { key: 6, id: 6, selected: false, onCheckChange: (id) => { console.log('change', id) } }, 'Literal html')
-        , h(Entry, { key: 7, id: 7, selected: false, onCheckChange: (id) => { console.log('change', id) } }, 'Literal html')
-        , h(ReactRouter.Router, {history: ReactRouter.browserHistory},
-            h(ReactRouter.Route, { path: '/', component: Entry }),
-            h(ReactRouter.Route, { path: 'test', component: Main })
-        )
     ),
     document.getElementById('content')
 );

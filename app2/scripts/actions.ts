21a6ef3b00db8dffa7e8b09fc951fc4534b2b236
Simon Peter Martin <smartin@scottlogic.com>
2016-06-23 15:19:35 +0100

    Add isomorphic fetch Add thunk middleware Add example thunking method

--- app2/scripts/actions.ts
+++ app2/scripts/actions.ts
@@ -1,3 +1,5 @@
+import 'isomorphic-fetch'
+
 const uid = (): string => Math.random().toString(34).slice(2)
 
 export interface TogglePayload {
@@ -33,14 +35,14 @@ export function addTodo(text: string): Action {
 export function toggleTodo(id: string): Action {
     return {
         type: 'TOGGLE_TODO',
-        payload: {id}
+        payload: { id }
     }
 }
 
 export function deleteTodo(id: string): Action {
     return {
         type: 'DELETE_TODO',
-        payload: {id}
+        payload: { id }
     }
 }
 
@@ -49,4 +51,28 @@ export function setMode(id: string, mode: number, pos: number): Action {
         type: 'SET_MODE',
         payload: { id, mode, pos }
     }
+}
+
+export const fetchPosts = (subreddit: string) => (dispatch: Function) => {
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
+
+    dispatch(addTodo(subreddit))
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return fetch(`http://localhost:3000/api/queue`)
+        .then(response => response.json())
+        .then(json =>
+            // We can dispatch many times!
+            // Here, we update the app state with the results of the API call.
+            setTimeout( () => dispatch(addTodo(JSON.stringify(json))), 1000)
+        )
+
+    // In a real world app, you also want to
+    // catch any error in the network call.
 }
--- app2/scripts/app.ts
+++ app2/scripts/app.ts
@@ -4,20 +4,24 @@ import {h} from 'react-markup'
 
 import {Provider} from 'react-redux'
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 
 import {List, Map} from 'immutable'
 
+import { fetchPosts } from './actions'
 import { Reducer as reducer } from './reducer'
 import { QueueList } from './containers'
 import { QueueEntry2 } from './queue_entry'
 
+import thunkMiddleware from 'redux-thunk'
+
 const Hammer = require('react-hammerjs')
 
-const store = createStore(reducer, (<any>window).devToolsExtension && (<any>window).devToolsExtension())
+const store = createStore(reducer, compose(applyMiddleware(thunkMiddleware), (<any>window).devToolsExtension ? (<any>window).devToolsExtension() : (f: any) => f))
 
 function handleTap() {
     console.log('tap', arguments)
+    fetchPosts('queue')(store.dispatch)
 }
 function handleSwipe() {
     console.log('swipe', arguments)
